a680093ca906f51c0506722cce3ace06ab9ca623
abramenko-alexey <abramenko_alex@mail.ru>
2016-08-24 13:06:49 +0700

    master

---
+++ Swift 10 Course/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Swift 10 Course
+//
+//  Created by Alexey on 24.08.16.
+//  Copyright © 2016 Abramenko Alexey. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
